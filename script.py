4ba82852c868448ad7cd1af49422597106650c75
Dajana Velichkovska <dajana.velichkovska@auroralabs.com>
2025-07-15 13:39:52 +0200

    Binary input

--- script.py
+++ script.py
@@ -2,22 +2,23 @@ import csv
 import requests
 import numpy as np
 from dateutil import parser
+from argparse import ArgumentParser
 
-def main():
+def parse_args():
+    parser = ArgumentParser()
+    parser.add_argument('-e', '--elf')
+    return parser.parse_args()
+
+def main(args):
     print("Running my Python script with dependencies! Some modification.")
     response = requests.get("https://httpbin.org/get")
     print(f"Response from httpbin: {response.status_code}")
     arr = np.array([1, 2, 3])
     print(f"Numpy array: {arr}")
-    print("Read data.csv")
-    with open('data.csv', 'r') as file:
-        for i, r in enumerate(csv.reader(file)):
-            if i == 0:
-                continue
-            print(r)
     date_str = "2025-07-14T10:00:00Z"
     parsed_date = parser.isoparse(date_str)
     print(f"Parsed date: {parsed_date}")
+    print(f"Elf: {args.elf}")
 
 if __name__ == "__main__":
-    main()
+    main(parse_args())
