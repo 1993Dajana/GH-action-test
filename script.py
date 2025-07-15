e7cd91b34e122068a02ce8b87e0a8fdf5335223f
Dajana Velichkovska <dajana.velichkovska@auroralabs.com>
2025-07-15 12:06:09 +0200

    Add dep

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import requests
 import numpy as np
-import yaml
+import csv
 
 def main():
     print("Running my Python script with dependencies! Some modification.")
@@ -8,6 +8,12 @@ def main():
     print(f"Response from httpbin: {response.status_code}")
     arr = np.array([1, 2, 3])
     print(f"Numpy array: {arr}")
-
+    print("Read data.csv")
+    with open('data.csv', 'r') as file:
+        for i, r in enumerate(csv.reader(file)):
+            if i == 0:
+                continue
+            print(r)
+        
 if __name__ == "__main__":
     main()
