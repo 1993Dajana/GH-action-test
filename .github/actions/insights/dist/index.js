1a8763cf2a4844b7b65df7fc96f9706b0f7f01e8
Dajana Velichkovska <dajana.velichkovska@gmail.com>
2025-07-18 03:55:13 +0200

    Final test

--- .github/actions/insights/dist/index.js
+++ .github/actions/insights/dist/index.js
@@ -27563,43 +27563,15 @@ async function run() {
     const project = core.getInput("project", { required: true });
     const version = core.getInput("version", { required: true });
 
-    core.startGroup(`Fetch function insights for ${project}(${version})`)
-    // let insights = "";
-    // const options = {};
-    // options.listeners = {
-    //   stdout: (data) => {
-    //     insights += data.toString();
-    //   },
-    // };
-    // await exec.exec("loci_api", ["func-insights", project, version], options);
-    const insights = `{
-        "project": "DIFF Tests",
-        "version": "case_03_v1",
-        "functions": [
-          {
-            "name": "example::foo",
-            "file": "src/foo.cpp",
-            "lines_of_code": 42,
-            "cyclomatic_complexity": 5
-          },
-          {
-            "name": "example::bar",
-            "file": "src/bar.cpp",
-            "lines_of_code": 30,
-            "cyclomatic_complexity": 3
-          },
-          {
-            "name": "example::baz",
-            "file": "src/baz.cpp",
-            "lines_of_code": 75,
-            "cyclomatic_complexity": 8
-          }
-        ],
-        "summary": {
-          "total_functions": 3,
-          "total_lines_of_code": 147
-        }
-      }`; 
+    core.startGroup(`Fetch function insights for ${project} (${version})`)
+    let insights = "";
+    const options = {};
+    options.listeners = {
+      stdout: (data) => {
+        insights += data.toString();
+      },
+    };
+    await exec.exec("loci_api", ["func-insights", project, version], options);
     core.info("Insights fetched successfully");
     core.endGroup();
 
