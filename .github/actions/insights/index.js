8a955eab696e3aaf3e3926f3b53b77019757e554
Dajana Velichkovska <dajana.velichkovska@gmail.com>
2025-07-18 03:38:41 +0200

    test

--- .github/actions/insights/index.js
+++ .github/actions/insights/index.js
@@ -6,9 +6,24 @@ async function run() {
     const project = core.getInput("project", { required: true });
     const version = core.getInput("version", { required: true });
 
-    core.startGroup(`Fetch function insights for ${project} ${version}`);
-    // await exec.exec("loci_api", ["func-insights", project, version]);
+    core.startGroup(`Fetch function insights ${project}(${version})`);
+    // let insights = "";
+    // const options = {};
+    // options.listeners = {
+    //   stdout: (data) => {
+    //     insights += data.toString();
+    //   },
+    // };
+    // await exec.exec("loci_api", ["func-insights", project, version], options);
+    const insights = {
+        project: 'Example',
+        binaries: ['bin1', 'bin2'],
+        mode: 'upload'
+    };      
     core.info("Insights fetched successfully");
+    const jInsights = JSON.parse(insights);
+    core.startGroup('Fetched Function Insights');
+    core.info(JSON.stringify(jInsights, null, 2));
     core.endGroup();
   } catch (err) {
     core.setFailed(`Insights fetch failed: ${err.message}`);
